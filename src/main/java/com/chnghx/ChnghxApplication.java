1ef5ecfff7fda07df831cfe19742ec3dd0fd3b9a
hnhx166 <986895336@qq.com>
2018-07-18 09:48:21 +0800

    删除

---
+++ src/main/java/com/chnghx/ChnghxApplication.java
@@ -0,0 +1,12 @@
+package com.chnghx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChnghxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChnghxApplication.class, args);
+	}
+}
